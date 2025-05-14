33196d53f62e39a95f293861acfa2b14df9d05c9
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-14 14:02:11 +0600

    entity: user ( elementary )

---
+++ server/src/main/java/com/example/firebase/demo/entities/User.java
@@ -0,0 +1,17 @@
+package com.example.firebase.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private boolean verified;
+    private String firebaseUid;
+}
