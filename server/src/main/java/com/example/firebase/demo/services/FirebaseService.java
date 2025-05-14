5ec287b29672755115b7f5c20fc88eadf6675ff1
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-14 13:52:38 +0600

    service: fire base token service

---
+++ server/src/main/java/com/example/firebase/demo/services/FirebaseService.java
@@ -0,0 +1,18 @@
+package com.example.firebase.demo.services;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseService {
+
+    public FirebaseToken verifyIdToken(String idToken) {
+        try {
+            return FirebaseAuth.getInstance().verifyIdToken(idToken);
+        } catch (FirebaseAuthException e) {
+            throw new RuntimeException("Invalid Firebase ID token", e);
+        }
+    }
+}
