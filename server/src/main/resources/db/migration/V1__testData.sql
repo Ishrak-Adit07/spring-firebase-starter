dd7b0e9232bc71af86b19f7cc92a749522892c1a
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-16 16:47:01 +0600

    sql: user test data

---
+++ server/src/main/resources/db/migration/V1__testData.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, full_name, password, role, phone_number)
+VALUES
+    ('john.doe@example.com', 'John Doe', '$2a$10$Ag/EVWeldbSzlxzMHWPdKezgYSL41TWXlcz7iNCiH5F9VCP/hRIcG', '1234567890'),
+    ('jane.smith@example.com', 'Jane Smith', '$2a$10$Ag/EVWeldbSzlxzMHWPdKezgYSL41TWXlcz7iNCiH5F9VCP/hRIcG',  '0987654321'),
+    ('admin@example.com', 'Admin User', '$2a$10$Ag/EVWeldbSzlxzMHWPdKezgYSL41TWXlcz7iNCiH5F9VCP/hRIcG', NULL);
