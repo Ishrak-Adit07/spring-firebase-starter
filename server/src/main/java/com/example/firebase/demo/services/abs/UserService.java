212253561cc247222a4abdea7636414f30f830d8
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-16 11:29:10 +0600

    service: abstraction - user service

--- server/src/main/java/com/example/firebase/demo/services/abs/UserService.java
+++ server/src/main/java/com/example/firebase/demo/services/abs/UserService.java
@@ -1,4 +1,10 @@
 package com.example.firebase.demo.services.abs;
 
+import com.example.firebase.demo.entities.User;
+
 public interface UserService {
+
+    User saveUser(User user);
+    User getUserById(Long userId);
+    void updateUserInfo(Long userId, String email, String phoneNumber);
 }
--- server/src/main/java/com/example/firebase/demo/services/impls/UserServiceImpl.java
+++ server/src/main/java/com/example/firebase/demo/services/impls/UserServiceImpl.java
@@ -3,28 +3,32 @@ package com.example.firebase.demo.services.impls;
 import com.example.firebase.demo.entities.User;
 import com.example.firebase.demo.repositories.UserRepository;
 import com.example.firebase.demo.repositories.UserVerificationRepository;
+import com.example.firebase.demo.services.abs.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserVerificationRepository userVerificationRepository;
 
+    @Override
     public User saveUser(User user) {
         userVerificationRepository.deleteUserVerInfoByEmail(user.getEmail());
         return userRepository.save(user);
     }
 
+    @Override
     public User getUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(
                 () -> new UsernameNotFoundException("User not found")
         );
     }
 
+    @Override
     public void updateUserInfo(Long userId, String email, String phoneNumber) {
         User user = getUserById(userId);
         user.setEmail(email);
