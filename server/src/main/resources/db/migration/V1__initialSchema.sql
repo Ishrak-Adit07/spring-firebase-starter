9e521ddca9d89ebc1cb2be82564819f8d21d079b
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-16 17:15:00 +0600

    attempt: v2 test data sql

--- server/src/main/resources/db/migration/V1__initialSchema.sql
+++ server/src/main/resources/db/migration/V1__initialSchema.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (email, full_name, password, role, phone_number)
+INSERT INTO users (email, full_name, password, phone_number)
 VALUES
     ('john.doe@example.com', 'John Doe', '$2a$10$Ag/EVWeldbSzlxzMHWPdKezgYSL41TWXlcz7iNCiH5F9VCP/hRIcG', '1234567890'),
     ('jane.smith@example.com', 'Jane Smith', '$2a$10$Ag/EVWeldbSzlxzMHWPdKezgYSL41TWXlcz7iNCiH5F9VCP/hRIcG',  '0987654321'),
